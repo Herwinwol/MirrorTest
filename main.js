d70727d558080dc97582cb7c3a8fa6271cfc3fab
Herwin Wolchuk <herwinwol22@gmail.com>
2022-10-04 10:54:27 -0300

    versionFinal

---
+++ main.js
@@ -0,0 +1,14 @@
+// ARRAY DE IMAGENES
+const galeria = [];
+
+// SLIDER c/ eventos
+
+let changeImg =>{
+
+};
+
+let slider = document.getElementById("slider");
+slider.addEventListener("click,changeImg");
+
+
+// OBJETO EDITOR DE CODIGO 
